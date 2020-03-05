a7234f048afce3bbb53081912b0f974d62e6214e
Brandon Gerbasi <brandon.gerbasi@gmail.com>
2020-03-05 07:48:25 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -13,16 +13,12 @@
 # 'I LOVE YOU TOO PUMPKIN!'
 
 def speak_to_grandma
-  if "I LOVE YOU GRANDMA!"
+  if phrase == "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
   elsif phrase != phrase.upcase()
     return HUH?! SPEAK UP, SONNY!
   else NO, NOT SINCE 1938!
 end
 end
-    
-    def speak_to_grandma
-      if "I LOVE YOU GRANDMA!"
-        return "I LOVE YOU TOO PUMPKIN!'
-        els
+ 
       
